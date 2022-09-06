8543b6e84df8ff719687ceb305502c46ed44230d
Eduard Litvinchuk <elitvinchuck@gmail.com>
2022-09-06 13:49:43 +0300

    Moved directions and ingredients to a separate table, added recipe date

--- src/main/java/com/elitvinchuck/recipes/models/Recipe.java
+++ src/main/java/com/elitvinchuck/recipes/models/Recipe.java
@@ -2,11 +2,15 @@ package com.elitvinchuck.recipes.models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -24,10 +28,23 @@ public class Recipe {
     @NotBlank
     private String description;
 
-    @Size(min = 1)
-    private ArrayList<String> ingredients;
+    @NotBlank
+    private String category;
 
     @Size(min = 1)
-    private ArrayList<String> directions;
+    @ElementCollection
+    @CollectionTable(name = "recipe_ingredients",
+            joinColumns = @JoinColumn(name = "recipe_id"))
+    private Set<String> ingredients = new HashSet<>();
+
+    @Size(min = 1)
+    @ElementCollection
+    @CollectionTable(name = "recipe_directions",
+            joinColumns = @JoinColumn(name = "recipe_id"))
+    private Set<String> directions = new HashSet<>();
+
+    @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate date;
 
 }
